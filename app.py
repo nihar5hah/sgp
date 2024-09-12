b0eece7e9b08ac4499c366634337fdb528b5db29
nihar shah <142441414+nihar5hah@users.noreply.github.com>
2024-09-13 00:41:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ def predict():
     return render_template('index.html', prediction_text=f'The URL is {result}')
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
